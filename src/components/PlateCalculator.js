93befa96da6bd782ec6f5475631119c1ad330da5
Steven Nielson <snielson222@gmail.com>
2024-08-02 11:42:30 -0400

    light mode and dark mode buttons up

--- src/components/PlateCalculator.js
+++ src/components/PlateCalculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import PlateDisplay from './PlateDisplay';
 import "./PlateCalculator.css";
 
@@ -6,12 +6,14 @@ const PlateCalculator = () => {
   const [weight, setWeight] = useState();
   const [plates, setPlates] = useState([]);
   const [unit, setUnit] = useState('lbs');
-  const [lightMode, setLightMode] = useState('darkMode');
+  const [lightMode, setLightMode] = useState(false);
 
   const plateOptions = {
     lbs: [45, 35, 25, 10, 5, 2.5], // Available plates in pounds
     kg: [20, 15, 10, 5, 2.5, 1.25], // Available plates in kilograms
   };
+
+  useEffect(() => {}, [lightMode]);
 
   const barbellWeight = unit === 'lbs' ? 45 : 20; // Assuming a 45 lb or 20 kg barbell
 
@@ -35,7 +37,8 @@ const PlateCalculator = () => {
 
   return (
     <div className={lightMode}>
-      <button onClick={() => setLightMode(lightMode === 'darkMode' ? 'lightMode' : 'darkMode')}>Toggle Light Mode</button>
+      <button hidden={lightMode} onClick={() => setLightMode(true)}>Toggle Light Mode</button>
+      <button hidden={!lightMode} onClick={() => setLightMode(false)}>Toggle Dark Mode</button>
       <img id="weightPlate" src="https://workout-development-network.s3.us-east-2.amazonaws.com/imageedit_2_6458890741.png" alt="weightPlate" />
       <form onSubmit={handleSubmit}>
         <label>
